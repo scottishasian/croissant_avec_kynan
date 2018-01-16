070adf39269b810044c2dd6a50b46898ad863c4a
Kynan <k.song@blueyonder.co.uk>
2018-01-16 14:03:14 +0000

    Set up server request.

--- src/InfoComponents/InfoBox.jsx
+++ src/InfoComponents/InfoBox.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
+import RecipeSelector from './RecipeSelector.jsx';
 
 class InfoBox extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //
-  //   }
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipies: []
+    }
+  }
+
+  componentDidMount() {
+    console.log('mounted');
+    const url = 'https://api.edamam.com/search'
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.send();
+
+    request.addEventListener('load', () => {
+      if(request.status !== 200) return;
+      const jsonString = request.responseText;
+      const recipes = JSON.parse(jsonString);
+      this.setState({recipies: recipes});
+      console.log(this.state.recipes);
+    })
+  }
 
   render() {
     return (
       <div className='info-box'>
-        Hello
+        <h1>Recipes</h1>
+        <RecipeSelector />
       </div>
     )
   }
---
+++ src/InfoComponents/RecipeSelector.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class RecipeSelector extends React.Component {
+
+  render() {
+    return (
+      <select id="recipies">Hello</select>
+    )
+  }
+}
+
+export default RecipeSelector;
