e7ba2410b3e8e4b12171e7b9da349ce5f86b2db4
Kynan <k.song@blueyonder.co.uk>
2018-01-16 13:32:28 +0000

    First class set up.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import InfoComponents from './InfoComponents/InfoBox.jsx';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <InfoComponents />
+    );
+  }
+}
+
+export default App;
---
+++ src/InfoComponents/InfoBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class InfoBox extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //
+  //   }
+  // }
+
+  render() {
+    return (
+      <div className='info-box'>
+        Hello
+      </div>
+    )
+  }
+
+
+}
+
+export default InfoBox;
